8c705e2d26477e8004ce89295f405ee49cf38d4c
DestMarco <marcolino1424@gmail.com>
2025-02-21 14:05:41 +0100

    ++

--- EsameReact/src/AeroportiList.jsx
+++ EsameReact/src/AeroportiList.jsx
@@ -4,14 +4,13 @@ import axios from "axios";
 const AeroportiList = () => {
   const [aeroporti, setAeroporti] = useState([]);
 
-  // Effettua la richiesta per ottenere gli aeroporti al caricamento della pagina
+  
   useEffect(() => {
     axios
-      .get("http://127.0.0.1:5004/query/1")  // Richiesta GET per ottenere tutti gli aeroporti
+      .get("http://127.0.0.1:5004/query/1")  
       .then((response) => setAeroporti(response.data))
       .catch((error) => console.error("Errore nel recupero degli aeroporti", error));
-  }, []);  // Esegui una sola volta al caricamento del componente
-
+  }, []);  
   return (
     <div>
       <h1>Lista degli Aeroporti</h1>
